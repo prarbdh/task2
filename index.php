79a2ca0ca8a6f8cf9fe0377504d4c401c7ce3cda
Prarbdh Ranjan <prarbdhranjan@gmail.com>
2016-09-16 18:27:25 +0530

    ctask 2

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<title> Login Page </title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+	
+</head>
+
+<body>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<form name="loginForm" class="form-horizontal" >
+				<h1 id="header" class="loginForm bg-success"  ><strong>MY PROJECT</strong></h1>
+				<label class="loginForm text-center "> Email </label>
+				<input type="text" id="email" class="loginForm form-control" /></br></br>
+				<label class="loginForm text-center " > Password</label>
+				<input type="password" id="password" class="loginForm form-control"/> </br>
+				<input type="submit" class="loginForm btn btn-primary btn-lg btn-block " value="SUBMIT" /> </br></br>
+				<label class="loginForm">Don't have account? </label> <a href="signup.php" class="loginForm">Signup!</a> </br>
+				<a class="loginForm">Forget password?</a>
+			</form>
+		</div>
+		<div class="col-md-4"></div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<title> Signup Page </title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+	
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<form name="signupForm">
+					<h1 id="header" class="signupForm bg-success" ><strong>SIGN UP!</strong></h1>
+					<label class="loginForm" class="text-center"> Email </label>
+					<input type="text" id="email" class="signupForm form-control" /></br></br>
+					<label class="signupForm" class="text-center" > Password</label>
+					<input type="password" id="password" class="signupForm form-control"/> </br></br>
+					<label class="signupForm" class="text-center"> Username </label>
+					<input type="text" id="username" class="signupForm form-control" /></br></br>
+					<input type="submit" class="btn btn-primary btn-lg btn-block" value="SUBMIT" class="signupForm"/> </br>
+					<label class="signupForm">If you already have an account, </label> <a href="index.php" class="loginForm">Sign in</a> </br>
+				</form>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+		
+	</div>	
+</body>
+</html>
